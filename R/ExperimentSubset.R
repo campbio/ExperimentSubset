3c58b22a4bc4a861dbcd9ffb22427c10910f74c0
irzamsarfraz <33160268+irzamsarfraz@users.noreply.github.com>
2020-07-25 07:58:23 +0500

    Inherited SingleCellExperiment

--- R/ExperimentSubset.R
+++ R/ExperimentSubset.R
@@ -1,7 +1,28 @@
-ExperimentSubset <- setClass(
-  Class = "ExperimentSubset",
-  slots = c(
-    rowIndex = "numeric",
-    colIndex = "numeric"
-  )
+library(SingleCellExperiment)
+.ExperimentSubset <- setClass("ExampleClass",
+                          slots= representation(
+                            rowVec="integer",
+                            colVec="integer",
+                            rowToRowMat="matrix",
+                            colToColMat="matrix",
+                            rowToColMat="matrix",
+                            colToRowMat="matrix"
+                          ),
+                          contains="SingleCellExperiment"
 )
+
+ExperimentSubset <- function(
+  rowVec=integer(0),
+  colVec=integer(0),
+  rowToRowMat=matrix(0,0,0),
+  colToColMat=matrix(0,0,0),
+  rowToColMat=matrix(0,0,0),
+  colToRowMat=matrix(0,0,0),
+  ...)
+{
+  se <- SingleCellExperiment(...)
+  .ExperimentSubset(se, rowVec=rowVec, colVec=colVec,
+                rowToRowMat=rowToRowMat, colToColMat=colToColMat,
+                rowToColMat=rowToColMat, colToRowMat=colToRowMat)
+}
+
